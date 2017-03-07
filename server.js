627c9c8c8864faaef8c67ac61fcbfa040b71ed25
iamnewhere <saurabh.jindwani@outlook.com>
2017-03-07 11:49:36 +0530

    dynamic music list fetch

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express =  require('express');
+const fs = require('fs');
+const express = require('express');
 const http = require('http');
 const socket_io = require('socket.io');
 const timesyncServer = require('timesync/server');
@@ -11,6 +12,24 @@ var control_io = socket_io(control_server);
 var user_app = express();
 var user_server = http.Server(user_app);
 var user_io = socket_io(user_server);
+
+function getFiles(dir){
+    fileList = [];
+ 
+    var files = fs.readdirSync(dir);
+    for(var i in files){
+        if (!files.hasOwnProperty(i)) continue;
+        file = [];
+        var name = files[i];	file.push(name);
+        var addr = '/resources/audio/' + files[i];	file.push(addr);
+        var fname = dir + '/' + files[i];
+        if (!fs.statSync(fname).isDirectory()){
+            fileList.push(file);
+        }
+    }
+    fileList.sort();
+    return fileList;
+}
 
 control_app.use(express.static(__dirname + '/'));
 user_app.use(express.static(__dirname + '/'));
@@ -35,23 +54,31 @@ setInterval(function(){
 	control_io.emit('user',cnt);
 },5000);
 
+flist=getFiles(__dirname + '/resources/audio');
+
 user_io.on('connection',function(socket){
 	//console.log('hello');
 	cnt+=1;
 	
+	console.log('user connected');
+	user_io.emit('flist',flist);
+
 	/*socket.on('ready',function(){
 		control_io.emit('ready');
 	});*/
 
 	socket.on('disconnect',function(){
-		// console.log('bye');
+		console.log('user bye');
 		cnt-=1;
 	});
 });
 
 control_io.on('connection', function(socket){
 
-	control_io.emit('ip',ip);
+	// control_io.emit('ip',ip);
+
+	console.log('server connected');
+	control_io.emit('flist',flist);
 
 	socket.on('play', function(msg){
 		user_io.emit('play',msg);
@@ -63,6 +90,10 @@ control_io.on('connection', function(socket){
 
 	socket.on('change',function(msg){
 		user_io.emit('change',msg);
+	});
+
+	socket.on('disconnect',function(){
+		console.log('server bye');
 	});
 
 });
